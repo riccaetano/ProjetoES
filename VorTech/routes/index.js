a4bc16b2e2e5e223176b6edf496b9e5acd347a41
riccaetano <48357710+riccaetano@users.noreply.github.com>
2019-05-10 11:24:12 +0100

    Add files via upload

---
+++ VorTech/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://user:user@ips-gwakx.gcp.mongodb.net/test?retryWrites=true";
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post("/login", (req, res, next) => {
+  console.log(req.body)
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+    const collection = client.db("VorTech").collection("User");
+    collection.find({}).toArray((err,result)=>{
+      console.log(res);
+      
+    })
+    // perform actions on the collection object
+    client.close();
+  });
+});
+
+module.exports = router;
