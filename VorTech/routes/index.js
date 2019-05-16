5d023aa0de38f2415ee256d7ca26f5cb25a2cca7
sajesus99 <sajesus99@users.noreply.github.com>
2019-05-16 09:45:08 +0100

    com

--- VorTech/routes/index.js
+++ VorTech/routes/index.js
@@ -102,49 +102,6 @@ router.post("/recuperarPass", (req, res, next) => {
 
 module.exports = router;
 
-// Requisitar
-
-// router.get('/', function (req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// router.post("/registarSala", (req, res, next) => {
-//   console.log(req.body)
-//   var numeroSala = req.body.numeroSala;
-//   var numeroIPS = parseInt(req.body.numeroIPS);
-//   var dataInicio = req.body.dataInicio;
-//   var dataFim = req.body.dataFim;
-//   const client = new MongoClient(uri, { useNewUrlParser: true });
-//   client.connect(err => {
-//     const collection = client.db("VorTech").collection("ClassRoom");
-//     const collection1 = client.db("VorTech").collection("User");
-//     const collection2 = client.db("VorTech").collection("Request");
-//     var query= {name: numeroSala};
-//     var query1= {numIps: numeroIPS}; 
-//     // perform actions on the collection object
-//     collection1.findOne(query1, function(err,result){
-//       if(err || !result) {
-//         res.redirect("requisitar.html");
-//         console.log(err);
-//       } else {
-//         collection.findOne(query, values, function(err,result){
-//           if(err || !result) {
-//             res.redirect("recuperarPassERROR.html");
-//             console.log(result)
-//             console.log(err);
-//           } else {
-//             collection2.insertOne({user: numeroIPS, classRoom: numeroSala, startDate: dataInicio, endDate: dataFim})
-//             res.redirect("index.html");
-//             console.log(result)
-//           }
-//         })
-//       }
-      
-//     })
-//   });
-// });
-
-
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
