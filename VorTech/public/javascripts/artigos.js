aef637476b32f3476477c306adf4fec1af24f9aa
sajesus99 <sajesus99@users.noreply.github.com>
2019-05-31 16:09:02 +0100

    teste ocorrencias

--- VorTech/public/javascripts/artigos.js
+++ VorTech/public/javascripts/artigos.js
@@ -18,4 +18,3 @@ $.get("./getArtigos", function (data) {
         });
     });
 }); 
-    
---
+++ VorTech/public/javascripts/ocorrencias.js
@@ -0,0 +1,20 @@
+function criarTabela(conteudo) {
+    var tr = document.createElement('tr');
+    for (var i = 0; i < conteudo.length; i++) {
+        var td = document.createElement('td');
+        td.textContent = conteudo[i];
+        tr.appendChild(td);
+    }
+    return tr;
+}
+
+$(() => {
+    $.get("./getOcorrencias", function (data) {
+        console.log(data);
+        data.forEach(result => {
+            document.getElementById("tbody").appendChild(criarTabela(
+                [result.incidentId, result.incidentDate, result.description, result.status, result.local, result.user, result.incidentCreate]
+            ));
+        });
+    });
+}); 
